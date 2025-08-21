19f950972aa1256aeb2f4ed964896723b5b37e9c
JuanPelinco <pelincojuan3@gmail.com>
2025-08-21 10:52:24 -0500

    sdsdll

---
+++ Asistencia/src/main/java/peru/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,8 @@
+package peru.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println(" HOLA MUNDO CIENTIFICO");
+        AsistenciaApplication.main(args);
+    }
+}
---
+++ Asistencia/src/test/java/peru/edu/upeu/asistencia/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package peru.edu.upeu.asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
