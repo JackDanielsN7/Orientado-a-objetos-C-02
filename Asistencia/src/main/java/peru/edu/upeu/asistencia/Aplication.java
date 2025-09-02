d3bcc8d4fe59ba31bdf34b48b7df17198373d5f6
JackDanielsN7 <jacobohocl88@gmail.com>
2025-09-02 09:11:15 -0500

    Delete Asistencia directory

--- Asistencia/src/main/java/peru/edu/upeu/asistencia/Aplication.java
+++
@@ -1,8 +0,0 @@
-package peru.edu.upeu.asistencia;
-
-public class Aplication {
-    public static void main(String[] args) {
-        System.out.println(" HOLA MUNDO CIENTIFICO");
-        AsistenciaApplication.main(args);
-    }
-}
